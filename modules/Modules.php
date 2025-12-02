121481fec2bc122c4622fd1ff01697bd982f2c8e
Félix Gray-Sylvain <felixgs2005@gmail.com>
2025-12-02 15:06:50 -0500

    Modules.php

--- modules/Modules.php
+++ modules/Modules.php
@@ -1,29 +1,41 @@
 <?php
 /**
  * Register all modules with dependency tree.
- *
- * @package MEE\Modules
- * @since ??
  */
 
 namespace MEE\Modules;
 
-if ( ! defined( 'ABSPATH' ) ) {
-	die( 'Direct access forbidden.' );
+if (!defined('ABSPATH')) {
+	die('Direct access forbidden.');
 }
 
-use MEE\Modules\StaticModule\StaticModule;
+use MEE\Modules\MovieCardModule\MovieCardModule;
 use MEE\Modules\HelloWorldModule\HelloWorldModule;
 use MEE\Modules\DemoCardModule\DemoCardModule;
 use MEE\Modules\DemoEtudiantModule\DemoEtudiantModule;
 
-
+/**
+ * 1. Add modules to dependency tree
+ */
 add_action(
 	'divi_module_library_modules_dependency_tree',
-	function ( $dependency_tree ) {
-		$dependency_tree->add_dependency( new StaticModule() );
-		$dependency_tree->add_dependency( new HelloWorldModule() );
-		$dependency_tree->add_dependency( new DemoCardModule() );
-		$dependency_tree->add_dependency( new DemoEtudiantModule() );
+	function ($dependency_tree) {
+		$dependency_tree->add_dependency(new MovieCardModule());
+		$dependency_tree->add_dependency(new HelloWorldModule());
+		$dependency_tree->add_dependency(new DemoCardModule());
+		$dependency_tree->add_dependency(new DemoEtudiantModule());
+	}
+);
+
+/**
+ * 2. REGISTER modules with Divi
+ */
+add_action(
+	'divi_module_library_register_modules',
+	function () {
+		MovieCardModule::register();
+		HelloWorldModule::register();
+		DemoCardModule::register();
+		DemoEtudiantModule::register();
 	}
 );
